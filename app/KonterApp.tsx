e119f5588affa97d7a811663ae43cd2deaa1eeea
Iwang007 <tukangngoprek31@gmail.com>
2026-02-18 07:23:38 +0700

    fix typescript downloadJSON types

--- app/KonterApp.tsx
+++ app/KonterApp.tsx
@@ -130,7 +130,7 @@ function todayISO() {
   return `${yyyy}-${mm}-${dd}`;
 }
 
-function downloadJSON(filename, data) {
+function downloadJSON(filename: string, data: unknown) {
   const blob = new Blob([JSON.stringify(data, null, 2)], { type: "application/json" });
   const url = URL.createObjectURL(blob);
   const a = document.createElement("a");
